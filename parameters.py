12015c862cf4d3901a02c90691f1688753f3c49f
Ignisolver <kaignacy@gmail.ocm>
2022-01-05 17:16:49 +0100

    add better saveing 2

--- parameters.py
+++ parameters.py
@@ -3,7 +3,6 @@ GROUPS_FILE = 'sample data/grupy_baza.csv'
 CLASSES_FILE = 'sample data/zajecia_baza.csv'
 ROOMS_FILE = 'sample data/sale_baza.csv'
 LECTURERS_FILE = 'sample data/prowadzacy_baza.csv'
-RESULT_FOLDER_NAME = 'DEFAULT'
 
 # weights
 LECTURER_WEIGHT = 20  # waga istotnosci funkcji celu prowadzacego wzgledem studenta (waga studenta = 1)
@@ -11,7 +10,7 @@ FUN_WEIGHTS = (1, 1, 1, 1)  # wagi skladowych funkcji celu
 
 # other parameters
 SECTIONS_AMOUNT = 3
-REASSIGN_TYPE = 'backtracking'  # backtracking/reconstruction/replacing
+REASSIGN_TYPE = 'reconstruction'  # backtracking/reconstruction/replacing
 STEP = 15
 MAX_ITER = 1000
 UTIME = 10  # jednostka czasu
