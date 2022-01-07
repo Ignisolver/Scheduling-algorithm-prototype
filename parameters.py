c8487dae3a8de9a935edb81de5f0331378fd3cd4
Ignisolver <kaignacy@gmail.ocm>
2022-01-07 21:33:27 +0100

    dobanie parameters.py

---
+++ parameters.py
@@ -0,0 +1,23 @@
+"""
+Test of replacing method of reassignment
+"""
+
+# --------------------PARAMETERS-----------------------
+
+# Structures files .csv
+GROUPS_FILE = 'sample data/grupy_baza.csv'
+CLASSES_FILE = 'sample data/zajecia_baza.csv'
+ROOMS_FILE = 'sample data/sale_baza.csv'
+LECTURERS_FILE = 'sample data/prowadzacy_baza.csv'
+RESULT_FOLDER_NAME = 'replace'
+
+# weights
+LECTURER_WEIGHT = 20  # waga istotnosci funkcji celu prowadzacego wzgledem studenta (waga studenta = 1)
+FUN_WEIGHTS = (1, 1, 1, 1)  # wagi skladowych funkcji celu (FO, FD, FP, FR) 
+
+# other parameters
+SECTIONS_AMOUNT = 3
+REASSIGN_TYPE = 'replace'  # backtracking/reconstruction/replacing/ignore
+STEP = 15
+MAX_ITER = 1000
+UTIME = 10  # jednostka czasu
